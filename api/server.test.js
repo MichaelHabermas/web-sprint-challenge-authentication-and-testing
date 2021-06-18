b4761c0f5d56cf70febc620d05ae93efc4525bb3
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-18 15:46:41 -0400

    tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,8 +1,7 @@
-// Write your tests here
-
 const request = require('supertest');
 const db = require('../data/dbConfig');
 const server = require('./server');
+const Users = require('./users/users-model');
 
 const joke1 = {
 	id: '0189hNRf2g',
@@ -16,6 +15,10 @@ const joke3 = {
 	id: '08xHQCdx5Ed',
 	joke: 'Why didn’t the skeleton cross the road? Because he had no guts.'
 };
+const user = {
+	username: 'Biff',
+	password: '12341234'
+};
 
 test('sanity', () => {
 	expect(true).toBeTruthy();
@@ -28,32 +31,54 @@ beforeAll(async () => {
 });
 beforeEach(async () => {
 	await db('users').truncate();
-	// await db.seed.run();
 });
 afterAll(async () => {
 	await db.destroy();
 });
 
-describe('Register Functions', () => {
-	describe('create new user', () => {
-		it('adds user to the db', async () => {
+describe('Entry Functions', () => {
+	describe('Register new user', () => {
+		it('adds user to the db, return correct status code 201', async () => {
 			// let jokes;
-			// await Users.create(user);
+			let registered = await request(server)
+				.post('/api/auth/register')
+				.send(user);
+
+			expect(registered.status).toBe(201);
+
+			// let status;
+			// Users.create(user)
+			// 	.then(newUser => {
+			// 		status = res.status;
+			// 	})
+			// 	.catch(next);
+			// expect(status).toBe(200);
 			// jokes = await db('jokes');
-			// expect(jokes).toHaveLength(14);
 			// await jokes.create(joke2);
 			// jokes = await db('jokes');
 			// expect(jokes).toHaveLength(15);
 		});
 
 		it('returns the correct user', async () => {
+			let registered = await request(server)
+				.post('/api/auth/register')
+				.send(user);
+
+			expect(registered.body.username).toBe('Biff');
 			// const joke = await Jokes.create(joke1);
 			// expect(joke).toMatchObject({ name: 'Betty', budget: 40000 });
 		});
 	});
 
 	describe('Login functions', () => {
-		it('deletes joke from the db', async () => {
+		it('logs user to the db, return correct status code 200', async () => {
+			await request(server).post('/api/auth/register').send(user);
+
+			let loggedUser = await request(server)
+				.post('/api/auth/login')
+				.send(user);
+			// expect(loggedUser.body.message).toEqual('welcome, Biff');
+			expect(loggedUser.status).toBe(200);
 			// const [id] = await db('jokes').insert(joke1);
 			// let joke = await db('jokes').where({ id }).first();
 			// expect(joke).toBeTruthy();
@@ -62,7 +87,37 @@ describe('Register Functions', () => {
 			// expect(joke).toBeFalsy();
 		});
 
-		it('respond with te deleted joke', async () => {
+		it('responds with correct message', async () => {
+			await request(server).post('/api/auth/register').send(user);
+
+			let loggedUser = await request(server)
+				.post('/api/auth/login')
+				.send(user);
+			expect(loggedUser.body.message).toEqual('welcome, Biff');
+			// await db('jokes').insert(joke1);
+			// let joke = await request(server).delete('/api/jokes/14');
+			// expect(joke.body).toMatchObject(joke1);
+		});
+	});
+
+	describe('Getting Jokes', () => {
+		it('return correct status code 200', async () => {
+			await request(server).post('/api/auth/register').send(user);
+
+			await request(server).post('/api/auth/login').send(user);
+
+			let jokes = await request(server).get('/api/jokes');
+
+			expect(jokes).toBe(200);
+			// const [id] = await db('jokes').insert(joke1);
+			// let joke = await db('jokes').where({ id }).first();
+			// expect(joke).toBeTruthy();
+			// await request(server).delete('/api/jokes/' + id);
+			// joke = await db('jokes').where({ id }).first();
+			// expect(joke).toBeFalsy();
+		});
+
+		it('responds with correct jokes', async () => {
 			// await db('jokes').insert(joke1);
 			// let joke = await request(server).delete('/api/jokes/14');
 			// expect(joke.body).toMatchObject(joke1);
